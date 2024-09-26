58c205f00fd5eca4d5a50bba2c370dcdb627b214
bagozza <bagozza_96@mail.ru>
2024-09-26 07:41:07 +0500

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,12 @@
+def add_everything_up(a, b):
+    try:
+        print(a + b)
+    except TypeError as exc:
+        print(str(a) + str(b))
+    finally:
+        print('Готово!')
+
+
+(add_everything_up(123.456, 'строка'))
+(add_everything_up('яблоко', 4215))
+(add_everything_up(123.456, 7))
