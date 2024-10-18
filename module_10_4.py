c0332aa91b7f58bc24347f14ad8d67ca75e2d0e8
bagozza <bagozza_96@mail.ru>
2024-10-18 18:22:07 +0500

    Add files via upload

---
+++ module_10_4.py
@@ -0,0 +1,171 @@
+# import threading
+# import time
+#
+#
+# def some_func():
+#     time.sleep(4)
+#     raise Exception
+#
+#
+# def thread_func():
+#     try:
+#         some_func()
+#     except Exception:
+#         print('Exception!!!')
+#
+#
+# t1 = threading.Thread(target=thread_func)
+# t2 = threading.Thread(target=thread_func)
+#
+# t1.start()
+# t2.start()
+#
+# t1.join()
+# t2.join()
+# import threading
+# import time
+#
+#
+# def some_func():
+#     time.sleep(4)
+#     raise Exception
+#
+#
+# def excepthook(args):
+#     print(args.thread.name)
+#
+#
+# threading.excepthook = excepthook
+# tr1 = threading.Thread(target=some_func)
+# tr2 = threading.Thread(target=some_func)
+#
+# tr1.start()
+# tr2.start()
+#
+# tr1.join()
+# tr2.join()
+# import threading
+# import queue
+# import time
+#
+#
+# def producer(queue):
+#     while True:
+#         message = 'ping'
+#         queue.put(message)
+#         time.sleep(1)
+#
+#
+# def worker(queue):
+#     while True:
+#         message = queue.get()
+#         print(message)
+#
+#
+# q = queue.Queue()
+# t1 = threading.Thread(target=producer, args=(q,))
+# t2 = threading.Thread(target=worker, args=(q,))
+#
+# t1.start()
+# t2.start()
+#
+# t1.join()
+# t2.join()
+import time
+
+
+class User:
+    def __init__(self, nickname: str, password, age: int):
+        self.nickname = nickname
+        self.password = hash(password)
+        self.age = age
+
+    def __repr__(self):
+        return self.nickname
+
+
+class Video:
+    def __init__(self, title: str, duration: int, time_now=0, adult_mode=False):
+        self.title = title
+        self.duration = duration
+        self.time_now = time_now
+        self.adult_mode = adult_mode
+
+
+class UrTube:
+    def __init__(self):
+        self.users = []
+        self.videos = []
+        self.current_user = None
+
+    def log_in(self, nickname, password):
+        for user in self.users:
+            if nickname == user.nickname and hash(password) == user.password:
+                self.current_user = user
+
+    def register(self, nickname, password, age):
+        n_user = User(nickname, password, age)
+        if n_user not in self.users:
+            user = User(nickname, password, age)
+            self.users.append(user)
+            self.current_user = user
+        else:
+            print(f'Пользователь {n_user} уже существует')
+
+    def log_out(self):
+        self.current_user = None
+
+    def add(self, *args):
+        videos_names = []
+        for video in self.videos:
+            videos_names.append(video.title)
+        for video in args:
+            if video.title not in videos_names:
+                self.videos.append(video)
+
+    def get_videos(self, search_word):
+        res = []
+        for video in self.videos:
+            if search_word.lower() in video.title.lower():
+                res.append(video.title)
+        return res
+
+    def watch_video(self, title):
+        if self.current_user is None:
+            print('Войдите в аккаунт, чтобы смотреть видео')
+            return
+        for video in self.videos:
+            if title in video.title:
+                if video.adult_mode and self.current_user.age < 18:
+                    print('Вам нет 18 лет, пожалуйста, покиньте страницу')
+                else:
+                    for i in range(1, video.duration + 1):
+                        print(i, end=" ")
+                        time.sleep(1)
+                    print('Конец видео')
+
+
+ur = UrTube()
+v1 = Video('Лучший язык программирования 2024 года', 200)
+v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+
+# Добавление видео
+ur.add(v1, v2)
+
+# Проверка поиска
+print(ur.get_videos('лучший'))
+print(ur.get_videos('ПРОГ'))
+
+# Проверка на вход пользователя и возрастное ограничение
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('vasya_pupkin', 'lolkekcheburek', 13)
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
+ur.watch_video('Для чего девушкам парень программист?')
+
+# Проверка входа в другой аккаунт
+ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
+print(ur.current_user)
+
+# Попытка воспроизведения несуществующего видео
+ur.watch_video('Лучший язык программирования 2024 года!')
