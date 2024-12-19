81087c8baf5ad104671b847c7b2e8786e55653a3
bagozza <bagozza_96@mail.ru>
2024-12-19 14:41:44 +0500

    Add files via upload

---
+++ module_16_2.py
@@ -0,0 +1,32 @@
+import uvicorn
+from fastapi import FastAPI, Path
+from typing import Annotated
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+@app.get("/")
+async def main_page() -> dict:
+    return {'message': 'Главная страница'}
+
+
+@app.get("/admin")
+async def admin() -> dict:
+    return {'message': 'Вы вошли как администратор'}
+
+
+@app.get("/user/{user_id}")
+async def id_user(user_id: int = Path(ge=1, le=100, description='Enter ur ID', example=1)) -> dict:
+    return {'message': f'Вы вошли как пользователь № {user_id}'}
+
+
+@app.get("/user/{username}/{age}")
+async def user_dict(
+        username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
+        age: int = Path(ge=18, le=120, description='Enter age', example=24)) -> dict:
+    return {f'Информация о пользователе. Имя': {username}, 'Возраст': {age}}
+
+
+if __name__ == "__main__":
+    uvicorn.run("module_16_2:app", host="127.0.0.1", port=8000, workers=4)
